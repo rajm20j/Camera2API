b963d6d1ec4388fdd41c1c4977dc09274a49053d
Mayank <rajm20j@gmail.com>
2019-05-26 04:45:13 +0530

    1

--- app/src/main/java/com/example/camera2api/MainActivity.java
+++ app/src/main/java/com/example/camera2api/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.camera2api;
 
+import android.graphics.SurfaceTexture;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.TextureView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnCapture;
+    private TextureView textureView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCapture=(Button)findViewById(R.id.capBtn);
+        textureView=(TextureView)findViewById(R.id.textureView);
+
+        assert textureView != null;
+
+        textureView.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
+            @Override
+            public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int i, int i1) {
+
+            }
+
+            @Override
+            public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int i, int i1) {
+
+            }
+
+            @Override
+            public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+                return false;
+            }
+
+            @Override
+            public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
+
+            }
+        });
     }
 }
