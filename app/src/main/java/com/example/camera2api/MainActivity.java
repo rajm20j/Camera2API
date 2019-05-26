17da4fd5a78985da8b67b794ea6a95ea15d7aa1a
Mayank <rajm20j@gmail.com>
2019-05-26 16:08:26 +0530

    2

--- app/src/main/java/com/example/camera2api/MainActivity.java
+++ app/src/main/java/com/example/camera2api/MainActivity.java
@@ -1,15 +1,53 @@
 package com.example.camera2api;
 
+import android.content.Context;
+import android.graphics.ImageFormat;
 import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.media.ImageReader;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Size;
+import android.util.SparseIntArray;
+import android.view.Surface;
 import android.view.TextureView;
+import android.view.View;
 import android.widget.Button;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btnCapture;
     private TextureView textureView;
+    private String cameraID;
+    private CameraDevice cameraDevice;
+    private CameraCaptureSession cameraCaptureSession;
+    private CaptureRequest.Builder captureRequestBuilder;
+    private Size imageDimension;
+    private ImageReader imageReader;
+
+    //ORIENTATION CHECK
+    private static final SparseIntArray ORIENTATIONS =new SparseIntArray();
+    static{
+        ORIENTATIONS.append(Surface.ROTATION_0,90);
+        ORIENTATIONS.append(Surface.ROTATION_90,0);
+        ORIENTATIONS.append(Surface.ROTATION_180,270);
+        ORIENTATIONS.append(Surface.ROTATION_270,180);
+    }
+
+    //SAVING THE FILE
+    private File file;
+    private static final int REQUEST_CAMERA_PERMISSION=200;
+    private boolean mFlashSupported;
+    private Handler mBackgroundHandler;
+    private HandlerThread mBackgroundThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
         textureView.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
             @Override
             public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int i, int i1) {
-
+                openCamera();
             }
 
             @Override
@@ -42,5 +80,41 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        btnCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                takePicture();
+            }
+        });
+    }
+
+    private void takePicture() {
+        if(cameraDevice==null)
+            return;
+
+        CameraManager manager=(CameraManager)getSystemService(Context.CAMERA_SERVICE);
+        try{
+            CameraCharacteristics characteristics=manager.getCameraCharacteristics(cameraDevice.getId());
+            Size[] jpegSizes =null;
+            if(characteristics != null)
+                jpegSizes=characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
+                        .getOutputSizes(ImageFormat.JPEG);
+
+            //CAPTURE CUSTOM SIZE
+            int width=640;
+            int height=480;
+            if(jpegSizes !=null && jpegSizes.length>0)
+            {
+                width=jpegSizes[0].getWidth();
+                height=jpegSizes[0].getHeight();
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void openCamera() {
     }
 }
